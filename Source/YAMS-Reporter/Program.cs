2d610373ef32dc8eb65f285768e6ec3daa10d644
Richard Benson <me@richardbenson.co.uk>
2013-08-15 20:52:53 +0100

    Add the YAMS reporter

---
+++ Source/YAMS-Reporter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace YAMS_Reporter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //We need Admin for almost everything in here
+            // Needs UAC elevation for webmin to run
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!hasAdministrativeRight)
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo();
+                processInfo.Verb = "runas";
+                processInfo.FileName = Application.ExecutablePath;
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch
+                {
+                    Environment.Exit(0);
+                }
+                Environment.Exit(0);
+            }
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
