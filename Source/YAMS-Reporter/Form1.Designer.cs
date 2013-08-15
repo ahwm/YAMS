1ccd54c0e199a9019e8690359088af49cfc228f4
Richard Benson <me@richardbenson.co.uk>
2013-08-15 21:24:50 +0100

    Link in reporter didn't do anything

--- Source/YAMS-Reporter/Form1.Designer.cs
+++ Source/YAMS-Reporter/Form1.Designer.cs
@@ -93,6 +93,7 @@
             this.linkLabel1.TabIndex = 5;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "http://yams.in/qa";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // textBox1
             // 
